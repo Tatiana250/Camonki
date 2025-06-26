8b1d5a7c7d35698bbfa25eeeae6eb35ab81ca885
Tatiana Pereyra <tati25pereyra@gmail.com>
2025-06-26 12:03:01 -0300

    Se agregan estilos

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,7 +1,8 @@
 export interface Product{
     id: number;
     name : string;
-    description: string;
+    breveDescripcion: string;
+    descripcionCompleta: string;
     price: number;
     img: string;
     esDestacado: boolean;
